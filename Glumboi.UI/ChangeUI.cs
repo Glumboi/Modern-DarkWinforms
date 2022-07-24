543f02303718b3e0472c641291db742b5eb97314
Glumboi <58010838+Glumboi@users.noreply.github.com>
2022-07-25 01:10:04 +0200

    File upload

--- Glumboi.UI/ChangeUI.cs
+++ Glumboi.UI/ChangeUI.cs
@@ -10,6 +10,7 @@ namespace Glumboi.UI
 {
     public class UIChanger
     {
+        //Must be called in protected override void OnHandleCreated(EventArgs e) in order to work properly
         public static void ChangeTitlebarToDark(IntPtr handle)
         {
             DarkTitle.ChangeTitleBarToDark(handle);
